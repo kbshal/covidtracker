632d99a3978829de6aa8b9c2dfb7126b71efda3d
bishalkharel <bishalkharel0000@gmail.com>
2021-04-26 17:34:53 +0000

    fixed bugs

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
+from colorama import Fore,Back,Style
 from cores.logo import Logo
-import cores.indentify
+from  cores.indentify import Identify
 import os,time,sys,time
-os.system('clear')
 
+
+def popup_message():
+    print()
+os.system('clear')
 banner=Logo()
 banner.logo()
+identify=Identify()
+json_data=identify.raw_data()
+print(f'''
+Your IP: {(json_data['ip'])}
+Country: {(json_data['country_name'])}
 
-
+''')
 
 time.sleep(1.5)
