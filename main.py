4a7a30e95f654c1b85a9b18c738c5f653eed93ee
BishalKharal <bishalkharel0000@gmail.com>
2021-04-27 08:00:22 +0000

    added clean interface

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def file():
     os.system("clear")
 
     def popup_message():
-        message = "In this difficulty situation plesase wear mask and stay healty 🙏🙏  " 
+        message = "In this difficulty situation please wear mask and stay healty 😷 \n " 
         for letter in message:
             sleep(0.1) # In seconds
             sys.stdout.write(Fore.YELLOW+letter)
@@ -20,6 +20,7 @@ def file():
     os.system('clear')
     banner=Logo()
     banner.logo()
+    time.sleep(2)
     identify=Identify()
     final_stats_data=[]
     stats_results=identify.covid_stats()
@@ -29,14 +30,20 @@ def file():
 
     json_data=identify.raw_data()
     list_len=len(final_stats_data)
-
     print(f'''
 Your IP: {(json_data['ip'])}
 Country: {(json_data["country_name"])}
 
 ''')
+    print(f'{Fore.BLUE}[+] {Fore.RED} Cases: {Fore.WHITE+str(final_stats_data[0])}\n')
+    print(f'{Fore.BLUE}[+] {Fore.RED} Today Cases: {Fore.WHITE+str(final_stats_data[1])}\n')
+    print(f'{Fore.BLUE}[+] {Fore.RED} Deaths: {Fore.WHITE+str(final_stats_data[2])}\n')
+    print(f'{Fore.BLUE}[+] {Fore.RED} Today Deaths: {Fore.WHITE+str(final_stats_data[3])}\n')
+    print(f'{Fore.BLUE}[+] {Fore.RED} Recovered: {Fore.WHITE+str(final_stats_data[4])}\n')
+    print(f'{Fore.BLUE}[+] {Fore.RED} Today Recovered: {Fore.WHITE+str(final_stats_data[5])}\n')
+    print(f'{Fore.BLUE}[+] {Fore.RED} Tests: {Fore.WHITE+str(final_stats_data[6])}\n')
+    print(f'{Fore.BLUE}[+] {Fore.RED} Active: {Fore.WHITE+str(final_stats_data[7])}\n')
     
-    time.sleep(1.5)
 
 
 if __name__=="__main__":
