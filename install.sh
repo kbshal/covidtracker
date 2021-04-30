1fc6251c96c34179ed20daa878b7ab07ca6f9226
BishalKharal <bishalkharel0000@gmail.com>
2021-04-30 13:55:10 +0000

    added bash scripts

--- install.sh
+++ install.sh
@@ -1,2 +1,9 @@
+# bash scripts to install dependencies
+# NOTE: please update your sudo because it has major vulnerability which can lead to compramisation of your system 
+# You can read CVE-2021-3156 for info about sudo bugs
+
+sudo apt-get install python3
+sudo apt-get install python3-pip
+sudo apt-get install 
 pip3 install -r requirements.txt
 
